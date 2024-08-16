5931eeb2dc5eda85bc44ce68c805cc527ead04ed
shreeraam-sys <shreeraamr@gmail.com>
2024-08-16 22:58:16 +0530

    added application test class

---
+++ src/test/java/com/shree/communityconnect/CommunityConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shree.communityconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
